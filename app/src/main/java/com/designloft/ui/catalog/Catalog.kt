0a5a73aa86c6d273a283fb283c78aa8ae65ce9d3
progersto <denis.babakov@natife.com>
2019-07-15 11:38:45 +0300

    Initial commit

---
+++ app/src/main/java/com/designloft/ui/catalog/Catalog.kt
@@ -0,0 +1,15 @@
+package com.designloft.ui.catalog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.designloft.R
+
+class Catalog  : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
